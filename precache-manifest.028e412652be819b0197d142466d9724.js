d8d986fcbc4c9f8ede742fd0af6f095681501f31
Salman <salman@salman.beeline>
2022-06-12 19:36:41 +0300

    Updates

---
+++ precache-manifest.028e412652be819b0197d142466d9724.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07a6fd9c8a373dab26f536f9981c39b",
+    "url": "/medium_clone/index.html"
+  },
+  {
+    "revision": "14e83c6d3561cd40243f",
+    "url": "/medium_clone/static/js/2.9b1a6aab.chunk.js"
+  },
+  {
+    "revision": "fa6ce3dd649f78c8e1bb",
+    "url": "/medium_clone/static/js/main.12dd16c8.chunk.js"
+  },
+  {
+    "revision": "56ba74c5aad511753a5c",
+    "url": "/medium_clone/static/js/runtime-main.c1feaae3.js"
+  }
+]);
